246417271b021abbf26582df4699bbb6b688cf49
krircc <krircc@aliyun.com>
2018-08-11 18:48:02 +0800

    index

--- .deploy.sh
+++ .deploy.sh
@@ -7,6 +7,6 @@ set -e
 if [[ "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" == "master" ]]; then
   cp CNAME kriry && cp LICENSE kriry
   git clone https://github.com/davisp/ghp-import.git &&
-  ./ghp-import/ghp_import.py -n -p -f -m "Documentation upload" -b master -r https://"$GH_TOKEN"@github.com/krircc/krircc.github.io.git kriry &&
+  ./ghp-import/ghp_import.py -n -p -f -m "Documentation upload" -b master -r https://"$KRIRY_TOKEN"@github.com/krircc/krircc.github.io.git kriry &&
   echo "Uploaded documentation"
 fi
