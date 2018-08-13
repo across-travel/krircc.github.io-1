4487b55533a4b543bcb4a98f04e54afdd6d4f9a5
krircc <krircc@aliyun.com>
2018-08-13 08:39:07 +0800

    blog

--- kriry/js/number.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-    let number = document.getElementById('btn');
-    console.log(number.innerHTML)
-    word = Number.parseInt(number.innerHTML)
-    number.innerHTML = word + 1 
-}
